5c159ee781fdd0cfca873606df40b3c3ad4c2a20
miguelperezmvp <72437027+miguelperezmvp@users.noreply.github.com>
2022-01-26 11:27:55 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -22,9 +22,9 @@ public class Cliente {
 	      
 		  
 		  
-	      ia = InetAddress.getByName("127.0.0.1");//get local host address
+	      ia = InetAddress.getByName("100.100.100.2");//get local host address
 	      
-	      client = new Socket(ia, 53336); //create socket (*@\clientBox{1+2)}@*)
+	      client = new Socket(ia, 9999); //create socket (*@\clientBox{1+2)}@*)
 	      
 	      os = client.getOutputStream(); //get stream to write to
 	      
